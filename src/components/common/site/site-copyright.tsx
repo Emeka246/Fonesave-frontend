2310be962ed8ff2b49da386d395a7a4ba2d81c23
Emeka <fonsave2025@gmail.com>
2025-11-27 14:49:07 +0100

    Update Email FoneOwner 2025.

--- src/components/common/site/site-copyright.tsx
+++ src/components/common/site/site-copyright.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
-import { ComponentProps } from 'react'
+import { ComponentProps } from "react";
 export function SiteCopyright(props: ComponentProps<"div">) {
   return (
-    <div {...props} className="text-center text-xs text-balance *:hover:[a]:underline space-x-6">
-     <span>
-      © {new Date().getFullYear()} FonSave.
-     </span>
-     <Link to="#">Terms of Service</Link>
-     <Link to="#">Privacy Policy</Link>
+    <div
+      {...props}
+      className="text-center text-xs text-balance *:hover:[a]:underline space-x-6"
+    >
+      <span>© {new Date().getFullYear()} FoneOwner.</span>
+      <Link to="#">Terms of Service</Link>
+      <Link to="#">Privacy Policy</Link>
     </div>
   );
 }
