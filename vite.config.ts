7fc60d10fb946de78878bbcc753b00f16ef3fe9b
Emeka <fonsave2025@gmail.com>
2026-06-17 15:08:12 +0100

    Add DigitalOcean frontend hostnam

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   root: ".", // ✅ ensures Vite uses the project root
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // no dot needed
@@ -15,14 +15,15 @@ export default defineConfig({
   server: {
     host: true,
     allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      'ff3c6906b21f.ngrok-free.app',
-      'orca-app-842yf.ondigitalocean.app',
-      'foneowner.com',
-      'www.foneowner.com',
-      'fonsave.com',
-      'www.fonsave.com'
+      "localhost",
+      "127.0.0.1",
+      "ff3c6906b21f.ngrok-free.app",
+      "squid-app-brwax.ondigitalocean.app",
+      "orca-app-842yf.ondigitalocean.app",
+      "foneowner.com",
+      "www.foneowner.com",
+      "fonsave.com",
+      "www.fonsave.com",
     ],
   },
   build: {
@@ -30,4 +31,4 @@ export default defineConfig({
     emptyOutDir: true, // ✅ forces fresh rebuild
     sourcemap: false,
   },
-})
+});
